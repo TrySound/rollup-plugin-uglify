ef77828832144bd07e0d494da37d4f1c1b67c668
Bogdan Chadkin <trysound@yandex.ru>
2017-05-19 17:04:37 +0300

    Migrate test to jest

--- test/test.js
+++ test/test.js
@@ -4,77 +4,75 @@ const minify = require('uglify-js').minify;
 const readFile = require('fs').readFileSync;
 const uglify = require('../');
 
-process.chdir('test');
-
-it('should minify', () => {
+test('minify', () => {
     return rollup({
-        entry: 'fixtures/unminified.js',
-        plugins: [ uglify() ]
+        entry: 'test/fixtures/unminified.js',
+        plugins: [
+            uglify()
+        ]
     }).then(bundle => {
-        const unminified = readFile('fixtures/unminified.js', 'utf-8');
-        const result = bundle.generate({
-            format: 'cjs'
-        });
-        assert.equal(result.code.trim(), minify(unminified, { fromString: true }).code.trim());
+        const result = bundle.generate({ format: 'cjs' });
+        expect(Object.keys(result)).toHaveLength(2);
+        expect(result.code).toEqual('"use strict";var a=5;a<3&&console.log(4);\n');
+        expect(result.map).toBeFalsy();
     });
 });
 
-it('should minify via uglify options', () => {
+test('minify via uglify options', () => {
     return rollup({
-        entry: 'fixtures/empty.js',
-        plugins: [ uglify({
-            output: { comments: 'all' }
-        }) ]
+        entry: 'test/fixtures/empty.js',
+        plugins: [
+            uglify({ output: { comments: 'all' } })
+        ]
     }).then(bundle => {
-        const result = bundle.generate({
-            banner: '/* package name */',
-            format: 'cjs'
-        });
-
-        assert.equal(result.code, '/* package name */\n"use strict";\n');
+        const result = bundle.generate({ banner: '/* package name */', format: 'cjs' });
+        expect(Object.keys(result)).toHaveLength(2);
+        expect(result.code).toEqual('/* package name */\n"use strict";\n');
+        expect(result.map).toBeFalsy();
     });
 });
 
-it('should minify with sourcemaps', () => {
+test('minify with sourcemaps', () => {
     return rollup({
-        entry: 'fixtures/sourcemap.js',
-        plugins: [ uglify() ]
+        entry: 'test/fixtures/sourcemap.js',
+        plugins: [
+            uglify()
+        ]
     }).then(bundle => {
-        const result = bundle.generate({
-            format: 'cjs',
-            sourceMap: true
-        });
-
-        assert.ok(result.map, 'has a source map');
-        assert.equal(result.map.version, 3, 'source map has expected version');
-        assert.ok(Array.isArray(result.map.sources), 'source map has sources array');
-        assert.equal(result.map.sources.length, 2, 'source map has two sources');
-        assert.ok(Array.isArray(result.map.names), 'source maps has names array');
-        assert.ok(result.map.mappings, 'source map has mappings');
+        const result = bundle.generate({ format: 'cjs', sourceMap: true });
+        expect(Object.keys(result)).toHaveLength(2);
+        expect(result.map).toBeTruthy();
     });
 });
 
-it('should allow passing minifier', () => {
-    const expectedCode = readFile('fixtures/plain-file.js', 'utf-8');
+test('allow passing minifier', () => {
+    const expectedCode = readFile('test/fixtures/plain-file.js', 'utf-8');
     const testOptions = {
         foo: 'bar'
     };
 
     return rollup({
-        entry: 'fixtures/plain-file.js',
-        plugins: [ uglify(testOptions, (code, options) => {
-            assert.ok(code, 'has unminified code');
-            assert.equal(code, expectedCode.trim(), 'expected file content is passed to minifier');
-            assert.ok(options, 'has minifier options');
-            assert.equal(options.foo, 'bar', 'minifier gets custom options');
-
-            return { code };
-        })]
+        entry: 'test/fixtures/plain-file.js',
+        plugins: [
+            uglify(testOptions, (code, options) => {
+                expect(code.trim()).toEqual(expectedCode.trim());
+                expect(options).toEqual(testOptions);
+                return { code };
+            })
+        ]
     }).then(bundle => {
-        const result = bundle.generate();
-
-        assert.ok(result.code, 'result has return code');
-        assert.equal(result.code.trim(), expectedCode.trim(), 'result code has expected content');
+        const result = bundle.generate({ format: 'es' });
+        expect(result.code.trim()).toEqual(expectedCode.trim());
     });
+});
+
+test('throw error on uglify fail', () => {
+    const result = rollup({
+        entry: 'test/fixtures/failed.js',
+        plugins: [
+            uglify()
+        ]
+    });
+    return expect(result.catch(e => e.message)).resolves.toMatch(/Unexpected token/);
 });
 
